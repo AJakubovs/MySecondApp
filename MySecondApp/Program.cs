6c53fc560f1bd394ccd0b7e92bc55c4ceffa10d6
Aleks <Aleks@DESKTOP-28PP4L4>
2021-08-09 12:12:47 +0300

    Aleks@DESKTOP-28PP4L4 fixed a bug

--- MySecondApp/Program.cs
+++ MySecondApp/Program.cs
@@ -31,7 +31,7 @@ namespace MySecondApp
         }
     }
 
-    [VehicleDescription(Description ="Му Rockin Suzuki")]
+    [VehicleDescription(Description ="Му Rocking Suzuki")]
     public class Moto
     {
         public string Name { get; set; }
